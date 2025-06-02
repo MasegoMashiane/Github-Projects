3ed1d6d98d04d65a29768b5085f1a49ca2bb06f9
Masego_Mashiane <mashianemlb@gmail.com>
2025-06-02 15:26:51 +0200

    Changes

--- C and Webdev tutorials/JS/Daily JS Challenges/duplicateFilter.js
+++ C and Webdev tutorials/JS/Daily JS Challenges/duplicateFilter.js
@@ -1,4 +1,39 @@
 let array = [2,2,3,4,4,5]
 let Array2 = []
+let counter = array.length
 function removeDuplicate(){
+//For loop which will repeat n number of times
+//n being equivalent to the length of the array
+
+    for(let i=0; array.length; i++){
+        Array2.push(array.shift())
+
+    counter --
+    
+        if (array[i] === Array2){
+            array[i].pop
+            Array2 = []
+            repeat()
+        }
+
+        else{
+            return counter
+        }
+    }
 }
+
+
+function repeat(){
+    //check if array is empty first before repeating the remove... function
+    if (counter > 0){
+        removeDuplicate()
+    }
+
+    else{
+        return array
+    }
+}
+
+
+let x = removeDuplicate()
+console.log(x)
