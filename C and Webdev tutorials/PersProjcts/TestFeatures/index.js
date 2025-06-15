7ff8867fad667a9dc8e3aed0ebb7d0c153d7ffb9
MasegoMashiane <mashianemlb@gmail.com>
2025-06-15 16:07:44 +0200

    b2

--- C and Webdev tutorials/PersProjcts/TestFeatures/index.js
+++ C and Webdev tutorials/PersProjcts/TestFeatures/index.js
@@ -1,15 +1,15 @@
+const popSound = document.getElementById('popSound');
+
 document.querySelectorAll('.petal').forEach(petal => {
     petal.addEventListener('click', () => {
       // Apply pop animation
       petal.classList.add('popping');
-  
+      popSound.play();
+
       // Wait for the pop to finish, then replace with butterfly
       setTimeout(() => {
         petal.classList.remove('petal', 'popping');
         petal.classList.add('butterfly');
       }, 300);
     });
-  });
-
-  //const popSound = document.getElementById('popSound');
-//popSound.play();
+});
