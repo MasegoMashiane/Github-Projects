ab2150807032c82c73b8a4a5f5dfe50ba6cda29a
Masego_Mashiane <mashianemlb@gmail.com>
2025-06-18 15:36:21 +0200

    Update duplicateFilter.js

--- C and Webdev tutorials/JS/Daily JS Challenges/duplicateFilter.js
+++ C and Webdev tutorials/JS/Daily JS Challenges/duplicateFilter.js
@@ -2,36 +2,37 @@ let array = [2,2,3,4,4,4,4,5,6,6]
 const lenOfOrigArray = array.length
 let array2 = []
 let array3 = []
-
 let counter = 0
 //find all the duplicates in the array and then remove them
 
-function removeDuplicate(){
+
+
+function repeat(){
 //For loop which will repeat n number of times
 //n being equivalent to the length of the array
     
     console.log(lenOfOrigArray)
 
     if (counter <= lenOfOrigArray){
-    console.log(`array3: ${array3}`)
+        console.log(`array: ${array3}`)
     } 
-
     
     array2 = []
     console.log(`array2 must be empty: ${array2} 
                 State of array: ${array}    
                 round:${counter}`)
-
-
     checkForDuplicates()
     
 }
 
-
 function checkForDuplicates(){
     counter++
+
     array2.push(array.shift())
 
+    if (counter === 1){
+        array3[0] = array2[0]
+    }
     console.log(`Array2 must have someting: ${array2} 
                  State of array: ${array}                 
                 round:${counter}`)
@@ -40,12 +41,14 @@ function checkForDuplicates(){
     for(let i = 0; i < array.length; i++){
         
         if (array[i] === array2[0]){
-            array3.push(array2[0])
-            removeDuplicate()
+            console.log('if enclosure')
+            repeat()
         }
 
-        if (array2[0] != array[i] ){
-            checkForDuplicates()
+        else if(array2[0] != array[i] ){
+            console.log('else if enclosure')
+            array3.push(array[i])
+            repeat()
         }
     }
 }
