7fe232188bec6f3707c64d423594710080be2988
MasegoMashiane <mashianemlb@gmail.com>
2025-06-23 18:42:28 +0200

    Update tut.js

--- C and Webdev tutorials/JS/canvasTut/canvasElement/tut1/tut.js
+++ C and Webdev tutorials/JS/canvasTut/canvasElement/tut1/tut.js
@@ -1,12 +1,27 @@
 const canvas = document.querySelector('canvas')
-console.log(canvas)
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d')
 
- 
-let r;
+let r = 10;
+let x = r;
+let dx = 0.5;
+let y = 5;
+let dy = 0.5;
+function animate() {
+    requestAnimationFrame(animate);
+    //ctx.clearRect(0, 0, canvas.width, canvas.height)
+    //ctx.clearRect(0,0, canvas.width, canvas.height)
+    ctx.beginPath();
+    ctx.arc(x, Math.random()*canvas.height, r, 0, Math.PI*2, false);
+    ctx.strokeStyle = 'blue';
+    ctx.stroke();
+    x += dx;
+}
+
+animate()
+/*let r;
 let g;
 let b;
 
@@ -30,4 +45,4 @@ for(let i = 0; i<10 ; i++){
     ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
     ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 30, 30)
     ctx.stroke();
-}
+}*/
