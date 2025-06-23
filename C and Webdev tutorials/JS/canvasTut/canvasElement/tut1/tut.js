d36c507400d1c43ddf41c1a3746f2291f86b6f1e
Masego_Mashiane <mashianemlb@gmail.com>
2025-06-23 15:53:53 +0200

    Update tut.js

--- C and Webdev tutorials/JS/canvasTut/canvasElement/tut1/tut.js
+++ C and Webdev tutorials/JS/canvasTut/canvasElement/tut1/tut.js
@@ -10,24 +10,55 @@ let r;
 let g;
 let b;
 
-for(let i = 0; i<10 ; i++){
-    r = Math.floor(Math.random()*256)
-    g = Math.floor(Math.random()*256)
-    b = Math.floor(Math.random()*256)
+let rad= 20
 
-    ctx.beginPath();
-    ctx.arc(Math.random()*canvas.width, Math.random()*canvas.height, 10, 0, Math.PI*2, false);
-    ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`
-    ctx.stroke();
+let x = Math.random()*canvas.width
+let y = Math.random()*canvas.height
 
-    ctx.beginPath();
-    ctx.moveTo(Math.random()*canvas.width, Math.random()*canvas.height);
-    ctx.lineTo(Math.random()*canvas.width, Math.random()*canvas.height);
-    ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`;
-    ctx.stroke();
+let dx = (Math.random()-0.5)*35
+let dy = (Math.random()-0.5)*35
 
-    ctx.beginPath()
-    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-    ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 30, 30)
-    ctx.stroke();
+function animate(){
+
+
+ctx.clearRect(0, 0, canvas.width, canvas.height)
+requestAnimationFrame(animate)
+
+ctx.beginPath();
+ctx.arc(x, y, rad, 0, Math.PI*2, false);
+ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`
+ctx.stroke();
+
+x+=dx
+y+=dy
+
+if (x + rad > canvas.width|| x + rad < 2*rad){
+    dx  = -dx
 }
+if (y + rad > canvas.height || y + rad < 2*rad){
+    dy  = -dy
+
+}
+}
+// animate()
+// for(let i = 0; i<10 ; i++){
+//     r = Math.floor(Math.random()*256)
+//     g = Math.floor(Math.random()*256)
+//     b = Math.floor(Math.random()*256)
+
+//     ctx.beginPath();
+//     ctx.arc(Math.random()*canvas.width, Math.random()*canvas.height, 10, 0, Math.PI*2, false);
+//     ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`
+//     ctx.stroke();
+
+//     ctx.beginPath();
+//     ctx.moveTo(Math.random()*canvas.width, Math.random()*canvas.height);
+//     ctx.lineTo(Math.random()*canvas.width, Math.random()*canvas.height);
+//     ctx.strokeStyle = `rgb(${r}, ${g}, ${b})`;
+//     ctx.stroke();
+
+//     ctx.beginPath()
+//     ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+//     ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.height, 30, 30)
+//     ctx.stroke();
+// }
