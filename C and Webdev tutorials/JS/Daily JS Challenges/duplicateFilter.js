6eee1fff4476dad2d445925011ad0154b93ed13e
MasegoMashiane <mashianemlb@gmail.com>
2025-07-17 20:21:13 +0200

    UPDATE

--- C and Webdev tutorials/JS/Daily JS Challenges/duplicateFilter.js
+++ C and Webdev tutorials/JS/Daily JS Challenges/duplicateFilter.js
@@ -1,61 +1,20 @@
-let array = ["a","a","b","a"]
-const lenOfOrigArray = array.length
+//find a  duplicate in an array
+
+let array = [2, 5, 4, 4 ,5, 6]
 let array2 = []
-let array3 = []
 let counter = 0
-//find all the duplicates in the array and then remove them
+function checkDuplicates(){
+    if (counter < array.length){
 
+        for (let i; i < array.length; i++){    
+            if (array[counter] === array[counter + 1]){
+                
+            }
 
+            if (counter + 1 === false){
 
-function repeat(){
-//For loop which will repeat n number of times
-//n being equivalent to the length of the array
-    
-    console.log(lenOfOrigArray)
-
-    if (counter <= lenOfOrigArray){
-        console.log(`array3: ${array3}`)
-    } 
-    
-    array2 = []
-    console.log(`array2 must be empty: ${array2} 
-                State of array: ${array}    
-                round:${counter}`)
-    checkForDuplicates()
-    
-}
-
-
-function checkForDuplicates(){
-    counter++
-    array2.push(array.shift())
-    console.log(array.length)
-
-    /*if (counter === 1){
-        array3[0] = array2[0]
-    }*/
-
-    console.log(`Array2 must have someting: ${array2} 
-                 State of array: ${array}                 
-                round:${counter}`)
-
-
-    for(let i = 0; i < array.length; i++){
-        
-        if(i === array.length){
-            if (array[i] === array2[0]){
-            console.log('if enclosure')
-            repeat()
+            }
         }
-    }
 
-        else if(array2[0] != array[i] ){
-            console.log('else if enclosure')
-            array3.push(array[i])
-            repeat()
-        }
     }
 }
-
-
-checkForDuplicates()
